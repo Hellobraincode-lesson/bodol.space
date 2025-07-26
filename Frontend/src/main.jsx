a2105e3a364efbd00c532911c2196ae614e93d4d
Baljir0901 <baljir0901@gmail.com>
2025-07-27 06:11:24 +0900

    fix: axios baseURL and deployment sync

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import axios from "axios";
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
 import "./index.css";
 import { ChakraProvider, extendTheme, ColorModeScript} from "@chakra-ui/react";
 import { mode } from "@chakra-ui/theme-tools";
