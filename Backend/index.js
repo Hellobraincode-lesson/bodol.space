4d8f0aaca02f41f5ec0984c3c562963ce4e125c5
Baljir0901 <baljir0901@gmail.com>
2025-07-27 06:32:33 +0900

    fix: add CORS middleware for frontend-backend communication

--- Backend/index.js
+++ Backend/index.js
@@ -1,40 +1,42 @@
-const express = require("express");
-const dotenv = require("dotenv");
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
 dotenv.config();
 const port = process.env.PORT || 3000;
-const mongoose = require("mongoose");
-const app=express();
+const mongoose = require('mongoose');
+const app = express();
+app.use(
+  cors({
+    origin: ['https://bodol-space.vercel.app', 'http://localhost:3000'],
+    credentials: true,
+  })
+);
 mongoose
-  .connect(process.env.MONGO_ATLAS_URI) 
+  .connect(process.env.MONGO_ATLAS_URI)
   .then(() => {
-    console.log("Connected to MongoDB");
+    console.log('Connected to MongoDB');
   })
   .catch((error) => {
-    console.error("Error connecting to MongoDB:", error);
+    console.error('Error connecting to MongoDB:', error);
   });
- 
 
-const cookieParser = require("cookie-parser");
-const userRouter = require("./Routes/userRoutes");
-const postRouter = require("./Routes/postRoutes");
-const replyRouter = require("./Routes/replyRoute");
-
+const cookieParser = require('cookie-parser');
+const userRouter = require('./Routes/userRoutes');
+const postRouter = require('./Routes/postRoutes');
+const replyRouter = require('./Routes/replyRoute');
 
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
- 
+app.use(express.static('public'));
 
-
-app.get('/', (req,res)=>{
-  res.send("running...")
-})
+app.get('/', (req, res) => {
+  res.send('running...');
+});
 
 app.use(userRouter);
 app.use(postRouter);
 app.use(replyRouter);
-
 
 app.listen(port, () => {
   console.log(`App is listening at port:${port}`);
